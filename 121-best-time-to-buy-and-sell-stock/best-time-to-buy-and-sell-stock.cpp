0ea95d0931c15dded78d0178ec57d405211d2f33
Piyush Sahoo <67003964+Piyush-sahoo@users.noreply.github.com>
2024-08-30 09:32:37 +0530

    Time: 85 ms (58.85%) | Memory: 96.1 MB (63.49%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& arr) {
+        int min=arr[0], ans = 0;
         int n = arr.size();
-        vector<int> sm(n, 9999), lr(n, -9999);
-        int sml = 9999, lar = -9999;
-        for(int i = 0; i<n; i++){
-            if(sml>arr[i]) sml = arr[i];
-            // if(lar>arr[i])lmr = arr[i];
-            if(sml<sm[i])sm[i] = sml;
+        
+
+        for(int i = 1; i<n; i++){
+
+            ans = max(ans, arr[i]-min);
+
+            if(min>arr[i])min = arr[i];
         }
 
-        for(int i = n-1; i>=0; i--){
-            if(lar<arr[i]) lar = arr[i];
-            // if(lar>arr[i])lmr = arr[i];
-            if(lar>lr[i])lr[i] = lar;
-        }
-
-        int maxx = 0; // Variable to store the maximum profit
-        for (int i = 0; i < n; i++) {
-            maxx = max(maxx, lr[i] - sm[i]); // Calculate the maximum profit
-        }
-        return maxx;
-
+        return ans;
         
     }
 };
